b61a81873e4b198a4efd749fd37bcc71ae3685c8
Jeremy1whx <jeremy1whx@gmail.com>
2026-05-11 08:48:58 +0000

    fix code

--- finance/Lock_Free_Ring_Buffer/spsc/Ringbuffer.h
+++ finance/Lock_Free_Ring_Buffer/spsc/Ringbuffer.h
@@ -6,7 +6,7 @@ template <typename T>
 class RingBuffer {
     public:
         explicit RingBuffer(size_t capacity) : capacity_(capacity), buffer_(capacity) {
-            assert ((capacity & (capacity - 1) == 0) && "capacity must be power of 2");
+            assert ((capacity & (capacity - 1)) == 0 && "capacity must be power of 2");
             head_.store(0, std::memory_order_relaxed);
             tail_.store(0, std::memory_order_relaxed);
         }
