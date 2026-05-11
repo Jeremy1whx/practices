af87feeb3409faaa07592d18da1e5e40845d5cd5
Jeremy1whx <jeremy1whx@gmail.com>
2026-05-11 07:48:30 +0000

    move files to dir spsc

--- finance/Lock_Free_Ring_buffer/test.cpp
+++ finance/Lock_Free_Ring_buffer/test.cpp
@@ -1,4 +1,8 @@
 #include <catch2/catch_all.hpp>
+#include <thread>
+#include <chrono>
+#include <iostream>
+
 #include "Ringbuffer.h"
 
 TEST_CASE("Basic push and pop") {
@@ -30,4 +34,61 @@ TEST_CASE("Buffer full and empty") {
     REQUIRE(rb.pop(value));
 
     REQUIRE_FALSE(rb.pop(value)); // should be empty
+}
+
+TEST_CASE("SPSC multithread test") {
+    RingBuffer<int> rb(1024);
+
+    const int N = 100000;
+
+    std::thread producer([&]() {
+        for (int i = 0; i < N; ++i) {
+            while (!rb.push(i)) {}
+        }
+    });
+
+    std::thread consumer([&]() {
+        int value;
+        for (int i = 0; i < N; ++i) {
+            while (!rb.pop(value)) {}
+        }
+    });
+
+    producer.join();
+    consumer.join();
+
+    SUCCEED();
+}
+
+TEST_CASE("Performance test") {
+    RingBuffer<int> rb(1024);
+
+    const int N = 1'000'000;
+
+    auto start = std::chrono::high_resolution_clock::now();
+
+    std::thread producer([&]() {
+        for (int i = 0; i < N; ++i) {
+            while (!rb.push(i)) {}
+        }
+    });
+
+    std::thread consumer([&]() {
+        int value;
+        for (int i = 0; i < N; ++i) {
+            while (!rb.pop(value)) {}
+        }
+    });
+
+    producer.join();
+    consumer.join();
+
+    auto end = std::chrono::high_resolution_clock::now();
+
+    double seconds = std::chrono::duration<double>(end - start).count();
+    double ops = N / seconds;
+
+    std::cout << "Throughput: " << ops << " ops/sec\n";
+
+    REQUIRE(ops > 1e6); // 1M ops/sec
 }
