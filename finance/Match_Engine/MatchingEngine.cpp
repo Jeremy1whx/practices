f823851294c4df9e21a145197eb779a7433e29db
Jeremy1whx <jeremy1whx@gmail.com>
2026-05-30 14:14:59 +0000

    fix match_sell for larger benchmark

--- finance/Match_Engine/MatchingEngine.cpp
+++ finance/Match_Engine/MatchingEngine.cpp
@@ -177,7 +177,9 @@ void MatchingEngine::match_sell(Order order) {
         if (logger_) {
             auto msg = format_trade(*trade);
             logger_->log(msg.c_str());
-        }
+        } else {
+            trade_count_.fetch_add(1, std::memory_order_relaxed);
+            trade_pool_.recycle_oldest();}
 
         publish_market_data(*trade);
 
